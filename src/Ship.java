36ccfea320d11f086be23bf363831a75107599e6
HappyRunner389 <nicktab20@gmail.com>
2023-09-21 14:16:51 -0500

    Created getters and setters for Ship Class for ship buld and ship name

--- src/Ship.java
+++ src/Ship.java
@@ -1,2 +1,21 @@
 public class Ship {
+    private String ShipName;
+   private String ShipBuild;
+
+    public void setShipName(String Sname){
+        ShipName = Sname;
+
+    }
+
+    public void setShipBuild(String Sbuild){
+        ShipBuild = Sbuild;
+    }
+
+    public String getShipBuild() {
+        return ShipBuild;
+    }
+
+    public String getShipName() {
+        return ShipName;
+    }
 }
