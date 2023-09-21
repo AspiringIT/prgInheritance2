6dfb8dc1399631d3c58dc4721bc209576e21666a
HappyRunner389 <nicktab20@gmail.com>
2023-09-21 14:39:51 -0500

    Created passenger variables and appropriate accesor and mutator methods. I also overrided the toSring method in the ship superclass

--- src/CruiseShip.java
+++ src/CruiseShip.java
@@ -1,2 +1,24 @@
-public class CruiseShip extends Ship{
+public class CruiseShip extends Ship {
+    int maxPass;
+
+    public void setMaxPass(int maxPass) {
+        this.maxPass = maxPass;
+    }
+
+    public int getmaxPass() {
+        return maxPass;
+    }
+
+    @Override
+    public String toString(){
+        System.out.println("The Ship's name is " + getShipName() +
+                "\nThe Maximum Passenger Capacity is: " + getmaxPass());
+
+        return null;
+    }
+
+
 }
+
+
+
