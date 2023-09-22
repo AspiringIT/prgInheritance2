62601ce125fc176ae286d049250bb937a48a9ce6
HappyRunner389 <nicktab20@gmail.com>
2023-09-22 09:42:03 -0500

    Started getting user input for CruiseShip

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,19 @@ public class Main {
         CruiseShip CruiseBoat = new CruiseShip();
         CargoShip CargoBoat = new CargoShip();
         Scanner keyboard = new Scanner(System.in);
-    System.out.println("Please enter the data for a generic ship." + "\nWhat is the Ship Name?");
-    String tempShipName = keyboard.nextLine();
-    System.out.println("When was the ship built?");
-    String tempShipBuild = keyboard.nextLine();
-    s.setShipName(tempShipName);
-    s.setShipBuild(tempShipBuild);
+
+        System.out.println("Please enter the data for a generic ship." + "\nWhat is the Ship Name?");
+        s.setShipName(keyboard.nextLine());
+
+        System.out.println("When was the ship built?");
+        s.setShipBuild(keyboard.nextLine());
+        arr.add(s);
+
+        System.out.println("Please enter the data for a Cruise Ship" +
+                "\nWhat is the maximum passenger capacity?");
+        CargoBoat.setMaxPass(keyboard.nextInt());
+
+
 
 
         arr.add(CruiseBoat);
